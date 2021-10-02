9d0a11f0eb6566ac974d731af0351157e1c5692a
Ekaterina <abashkina.k@gmail.com>
2021-10-03 00:12:53 +0300

    add hero page,back button

--- pages/character/_id.vue
+++ pages/character/_id.vue
@@ -1,3 +1,62 @@
 <template>
-   <h1>id</h1>
+<div class="container hero__container">
+   <nuxt-link to='/' class='hero__link'>
+   Назад
+   </nuxt-link>
+   <h1 class="title">{{getHero.name}}</h1>
+   <p class="hero__text">{{getHero.location.name}}</p>
+   <p class="hero__text">{{getHero.species}}</p>
+  <img class="hero__img" :src="getHero.image" alt="">
+</div>
+   
 </template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+  name: 'Hero',
+  methods: {
+    ...mapActions('hero', [
+      'fetchHero'
+    ]),
+    },
+  mounted () {
+    this.fetchHero(this.$route.params.id)
+  },
+  computed: {
+    ...mapGetters('hero', [
+      'getHero'
+    ]),
+    
+  },
+  
+}
+  
+
+</script>
+
+<style>
+.hero__container{
+   display: flex;
+   flex-direction: column;
+   justify-content: flex-start;
+}
+.hero__link{
+   display: block;
+   color: #333;
+   margin-bottom: 20px;
+   font-size: 20px;
+}
+.hero__text{
+color:blue;
+margin-bottom: 20px;
+font-size: 40px;
+}
+.hero__img{
+   width: 600px;
+   height: 600px;
+   object-fit: contain;
+}
+</style>
+
---
+++ store/hero.js
@@ -0,0 +1,31 @@
+export const state = () => ({
+   hero: {}
+ })
+  
+ export const mutations = {
+   SET_HERO (state, data) {
+     state.hero = data
+   }
+ }
+  
+ export const actions = {
+   async fetchHero ({ commit }, id) {
+       
+         let apiUrl = `https://rickandmortyapi.com/api/character/${id}`
+  
+        await this.$axios.get(apiUrl).then(response => {
+         
+         let hero = response.data
+         
+         commit('SET_HERO', hero)
+         
+             })
+   }
+ }
+  
+ export const getters = {
+   getHero (state) {
+     return state.hero
+   }
+ }
+ 
