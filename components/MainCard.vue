730165abd56b91f72db41b5f0855f13796351387
Ekaterina <abashkina.k@gmail.com>
2021-10-02 13:06:16 +0300

    init commit

---
+++ components/MainCard.vue
@@ -0,0 +1,58 @@
+<template>
+   <div class="card">
+  <img :src="character.image" alt="Avatar" style="width:100%">
+  <div class="card__container">
+    <nuxt-link 
+    :to="`/character/${character.id}`">
+    {{character.name}}
+    </nuxt-link>
+    <h4 class="card__title"><b>{{character.name}}</b></h4>
+    <p>{{character.species}}</p>
+    <p>{{character.status}}</p>
+     <p>{{info.name}}</p>
+     <p>{{info[0]}}</p>
+    </div>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  props: ['hero'],
+  data() {
+    return {
+      info: '',
+      character: this.hero
+    }
+},
+
+// mounted() {
+//   axios.get(`${this.character.episode[1]}`)
+//     .then(response => this.info = response);
+// }
+ 
+}
+</script>
+
+
+<style>
+.card {
+    box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+    transition: 0.3s;
+    width: 300px;
+    height: 400px;
+}
+.card__title{
+  width: 100%;
+  margin-bottom: 10px;
+}
+
+.card:hover {
+    box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.card__container {
+    padding: 2px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <section class="container">
+    <div>
+        <h1 class="title">
+       Персонажи мультфильма
+      </h1>
+      <ul>
+       
+       <card v-for="hero in data.results"
+         :key=hero.id
+         :hero="hero"/> 
+      </ul>
+      
+      </div>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+import Card from '@/components/MainCard'
+
+export default {
+  components: {
+    Card
+  },
+  name: 'Main',
+   data () {
+            return {
+                characters: []
+            }
+        },
+  
+ async asyncData() {
+     const { data } = await axios.get(
+      'https://rickandmortyapi.com/api/character'
+    );
+        return {data} 
+  }  
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 100;
+  font-size: 50px;
+  color: #35495e;
+  margin-bottom: 20px;
+}
+
+ul {
+  padding: 0;
+  list-style-type: none;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 30px;
+}
+
+</style>
+
